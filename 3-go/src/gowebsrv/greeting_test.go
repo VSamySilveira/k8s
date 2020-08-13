5e6ec05094c54024e0bcd8d19f445c05cc3b2938
VSamySilveira <vinicius.silveira@gmail.com>
2020-08-13 18:12:47 -0300

    seventh testing attempt debug

--- 3-go/src/gowebsrv/greeting_test.go
+++ 3-go/src/gowebsrv/greeting_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestGreeting(t *testing.T) {
 	resultado := greeting("Olá!")
-	if resultado != "<b>Olá</b>" {
-	  t.Errorf("Greeting esperada: <b>Olá!</b>, obtida: %s", resultado)
+	if resultado != "<b>Olá<b>" {
+	  t.Errorf("Greeting esperada: <b>Olá!<b>, obtida: %s", resultado)
 	}
 }
